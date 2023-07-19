5fb2540aff636d05e6e7ad5d830e03f05443d8d4
Paulo Guilherme <paulinnho_testes_csharp@hotmail.com>
2023-07-18 21:27:00 -0300

    TASK13 - Creates tests for RegisterUser validations - Create RegisterUserRequestBuilder to generate user tests - Create ValidateUserTest to test for possible errors #13

---
+++ tests/TestUtility/Requests/RegisterUserRequestBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Bogus;
+using SharedRecipe.Reporting.Requests;
+
+namespace TestUtility.Requests
+{
+    public class RegisterUserRequestBuilder
+    {
+        public static UserRequestJson GenerateUserRequest(int lengthPassword = 10)
+        {
+            return new Faker<UserRequestJson>()
+                .RuleFor(r => r.Name, f => f.Person.FullName)
+                .RuleFor(r => r.Email, f => f.Person.Email)
+                .RuleFor(r => r.Phone, f => f.Phone.PhoneNumber("## ! ####-####").Replace("!", $"{f.Random.Int(min: 1, max: 9)}"))
+                .RuleFor(r => r.Password, f => f.Internet.Password(lengthPassword));
+        }
+    }
+}
